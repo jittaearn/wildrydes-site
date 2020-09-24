b00d2f2d768829e2f813e6da58037d13e7c7b068
jittaearn <jitta.k@ku.th>
2020-09-24 17:59:20 +0700

    update config.jss

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_nynsUZlBg', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '148actgdkpa9vh9medn604a204', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-southeast-1' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://asqjnozvff.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
